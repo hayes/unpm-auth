07ee3dadb3a2c81256af0967e62dc6aa134fadfc
jesse keane <me@jessekeane.me>
2016-04-13 12:44:13 -0700

    Add logging for auth failures

--- test/check-auth.js
+++ test/check-auth.js
@@ -31,3 +31,33 @@ test('checks', function(t) {
     }
   )
 })
+
+test('logs when auth fails', function(t) {
+  var credentials = new Buffer('username:hunter2').toString('base64')
+  var basicAuth = {authorization: 'Basic: ' + credentials}
+
+  check(
+    {req: {method: 'POST', url: '/a', headers: basicAuth}},
+    {method: 'POST', route: '/a'},
+    {config: {auth: {whitelist: list}, User: {auth: checkAuth}, log: {info: checkLog}}},
+    function() {
+      t.end()
+    }
+  )
+
+  function checkAuth (username, password, ready) {
+    t.equal(username, 'username')
+    t.equal(password, 'hunter2')
+    
+    process.nextTick(function () {
+      ready(null, null)
+    })
+  }
+
+  function checkLog (info) {
+    t.equal(info.username, 'username')
+    t.true(info.message.indexOf('authorization') > -1, 'message mentions auth')
+
+    t.end()
+  }
+})
