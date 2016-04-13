469e973139d668703a49ef9c53854182c715b23a
jesse keane <me@jessekeane.me>
2016-04-13 12:44:13 -0700

    Add logging for auth failures

--- test/check-auth.js
+++ test/check-auth.js
@@ -31,3 +31,40 @@ test('checks', function(t) {
     }
   )
 })
+
+test('logs when auth fails', function(t) {
+  t.plan(5)
+
+  var credentials = new Buffer('username:hunter2').toString('base64')
+  var basicAuth = {authorization: 'Basic: ' + credentials}
+  var fakeResponse = {writeHead: checkHead, end: noop}
+
+  check(
+    {req: {method: 'POST', url: '/b', headers: basicAuth}, res: fakeResponse},
+    {method: 'POST', route: '/b'},
+    {User: {auth: checkAuth}, config: {auth: {whitelist: list}}, log: {info: checkLog}},
+    noop
+  )
+
+  function checkHead (code) {
+    t.equal(code, 403)
+  }
+
+  function checkAuth (username, password, ready) {
+    t.equal(username, 'username')
+    t.equal(password, 'hunter2')
+    
+    process.nextTick(function () {
+      ready(null, null)
+    })
+  }
+
+  function checkLog (info) {
+    t.equal(info.username, 'username')
+    t.true(info.message.indexOf('authorization') > -1, 'message mentions auth')
+  }
+})
+
+function noop () {
+  // no-op
+}
